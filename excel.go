1e6e72e6cdf8eee1590fc0a96c3f31bf888d73ab
ahad3333 <akhadillokhabibullaev@gmail.com>
2023-05-08 11:12:56 +0800

    excel

---
+++ excel.go
@@ -0,0 +1,11 @@
+package exccel
+
+func getExcelColumnLetters(n int) []string {
+    letters := []string{}
+    for n > 0 {
+        n--
+        letters = append([]string{string(n%26 + 65)}, letters...)
+        n /= 26
+    }
+    return letters
+}
